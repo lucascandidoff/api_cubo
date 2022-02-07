7e9c72dc05a7e540441c1c11e3fcd6264ff5efbd
lucascandidoff <lucascandidoff@gmail.com>
2022-02-06 22:47:27 -0300

    testes com o selenium

--- sources/resources/cubo.py
+++ sources/resources/cubo.py
@@ -1,80 +1,112 @@
 from time import sleep
-from unicodedata import name
 from flask import Blueprint
 from sources.utils.response import Response
 from sources.business.cubo import CuboBusiness
 import requests
-from bs4 import BeautifulSoup as bs
-# from selenium import webdriver
-# from selenium.webdriver.chrome.options import Options  
-# from selenium.webdriver.support.ui import Select
+from bs4 import BeautifulSoup   
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options  
+from selenium.webdriver.support.ui import Select
 import os
-from playwright.sync_api import sync_playwright
+
+
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_argument('--disable-extensions')
+chrome_options.add_argument('--profile-directory=Default')
+chrome_options.add_argument("--incognito")
+chrome_options.add_argument("--disable-plugins-discovery");
+chrome_options.add_argument("--start-maximized")
+chrome_options.add_argument('--disable-blink-features=AutomationControlled')
+# chrome_options.add_argument("--headless")
+simp_path = 'chromedriver'
+abs_path = os.path.abspath(simp_path)
+os.chmod(abs_path,755)
+driver = webdriver.Chrome(executable_path=abs_path,options=chrome_options)
+driver.delete_all_cookies()
+driver.set_window_size(800,800)
+driver.set_window_position(0,0)
+#Remove navigator.webdriver Flag using JavaScript
+driver.execute_script("Object.defineProperty(navigator, 'webdriver', {get: () => undefined})")
+print('arguments done')
+# driver.get('https://www.google.com')
+driver.get('https://www.crunchbase.com/organization/cortex-intelligence')
+sleep(5)
+page = driver.page_source
+
+soup = BeautifulSoup(page, 'html.parser')
+
+description = soup.find('span', attrs={'class':'description'}).next
+
 
 cubo_api = Blueprint('cubo', __name__)
 cubo_business = CuboBusiness()
 header = { "Authorization" : "Basic aXRicm9rZXI6cmVrb3JidGkz" }
 
-# chrome_options = Options()
-# chrome_options.add_argument("--headless") # faz com que o browser não abra durante o processo
-# simp_path = 'chromedriver'
-# abs_path = os.path.abspath(simp_path)
-# os.chmod(abs_path,755)
-# driver = webdriver.Chrome(executable_path=abs_path,options=chrome_options)
 
 @cubo_api.route('/getfullcnpj/<int:cnpj>', methods=['GET'], strict_slashes=False)
 def find_all(cnpj):
 
-    with sync_playwright() as p:
-        browser = p.chromium.launch()
-        page = browser.new_page()
-        page.goto("https://www.crunchbase.com/v4/data/autocompletes?query=cortex-intelligence&collection_ids=organizations&limit=25")
+    # with sync_playwright() as p:
+    #     browser = p.chromium.launch()
+    #     page = browser.new_page()
+    #     result = page.goto("https://www.crunchbase.com/v4/data/autocompletes?query=cortex-intelligence&collection_ids=organizations&limit=25")
 
-        verificacao = page.query_selector('.page-title').inner_text()        
+    #     # result = page.evaluate()  # page.query_selector('.page-title').inner_text()        
    
-        tentativas = 0;
+    #     verificacao = page.query_selector('.page-title').inner_text()
+
+    #     tentativas = 0;
         
-        while(verificacao == "Please verify you are a human"):
-            print("Resolvendo captcha tentativas ", tentativas);
-            sleep(5);
-            page.screenshot(path='ss.png');
-            page.mouse.move(0, 0);
-            page.mouse.move(150, 240);
-            page.mouse.down();
-            sleep(1);
-            page.screenshot(path='ss2.png');
-            page.mouse.up();
-            sleep(5);
-            page.screenshot(path='ss3.png');
-            verificacao = page.query_selector('.page-title').inner_text()        
+        # while(verificacao == "Please verify you are a human"):
+        #     print("Resolvendo captcha tentativas ", tentativas);
+        #     sleep(5);
+        #     page.screenshot(path='ss.png');
+        #     page.mouse.move(0, 0);
+        #     page.mouse.move(150, 240);
+        #     page.mouse.down();
+        #     sleep(7);
+        #     page.screenshot(path='ss2.png');
+        #     page.mouse.up();
+        #     sleep(10);
+        #     page.screenshot(path='ss3.png');
+        #     verificacao = page.query_selector('.page-title').inner_text()     
 
-            tentativas += 1
+        #     tentativas += 1
 
-            if tentativas > 2: 
-                page.close()
-                break
+        #     if tentativas > 2: 
+        #         page.close()
+        #         break
 
-        browser.close()
+    #     if tentativas <= 2:
+    #         result = page.content()
 
-    
-    return "" 
+    #     browser.close()
 
+    # return result
 
+    driver = webdriver.Chrome(executable_path=abs_path,options=chrome_options)
     # driver.get('https://www.crunchbase.com/v4/data/autocompletes?query=cortex-intelligence&collection_ids=organizations&limit=25')
 
-    # page = driver.page_source
+    # json_company = driver.page_source
+
+    driver.get('https://www.crunchbase.com/organization/cortex-intelligence')
+
+    driver.close()
+
 
     # result = requests.get(f'https://www.crunchbase.com/v4/data/autocompletes?query=cortex-intelligence&collection_ids=organizations&limit=25');
     
     # page = bs(result.text,"html.parser")
     # if page.title:
-    #     if page.title.string == ''
-
+    #     if page.title.string == '':
 
     # html = open('page.html', 'w')
     # html.write(page)
     # html.close()
     
+    return page
+
+
     # speedio = requests.get(f'http://api-es.speedio.com.br:5003/getfullcnpjdata?cnpj={cnpj}',headers=header).json()
     
     # if 'WEBSITE' in speedio:
